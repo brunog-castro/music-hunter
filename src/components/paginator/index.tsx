956d15aa4ac26f524cc0add30cf35dd27d5a3698
Bruno Gonçalves <brunogc.goncalves@gmail.com>
2022-08-27 06:57:06 -0300

    feat: adding paginator to use with search results

---
+++ src/components/paginator/index.tsx
@@ -0,0 +1,70 @@
+import './styles.scss';
+
+interface PropTypes {
+    totalHits: number;
+    currentPage: number;
+    itemsPerPage: number;
+    invertedColors?: boolean;
+    onChange(idx: number): void;
+}
+
+export default function Paginator(props: PropTypes) {
+    const firstPage = 0;
+    const pageIndexesVisible = 9;
+    const totalPages = Math.ceil(props.totalHits / props.itemsPerPage);
+
+    const renderPageButtons = () => {
+        const pagination = [];
+        let startIndex = props.currentPage - 4;
+        startIndex = startIndex < (totalPages - pageIndexesVisible) 
+            ? startIndex 
+            : totalPages - pageIndexesVisible;
+        startIndex = startIndex < firstPage ? firstPage : startIndex;
+
+        let endIndex = startIndex + pageIndexesVisible;
+        endIndex = endIndex > totalPages ? totalPages : endIndex;
+        for (let i = startIndex; i <= endIndex; i++) {
+            pagination.push(
+                <button
+                    key={i}
+                    onClick={() => props.onChange(i)}
+                    className={i === props.currentPage ?
+                        "active paginate-button btn" :
+                        "paginate-button btn"
+                    }
+                >
+                    {i + 1}
+                </button>
+            );
+        }
+
+        return pagination;
+    };
+
+    return (
+        <div 
+            className={
+                "paginator-container" + 
+                (props.invertedColors ? " inverted" : "")
+            }
+        >
+            <div className="paginator">
+                <button
+                    className="prev paginate-button"
+                    disabled={props.currentPage <= firstPage}
+                    onClick={() => props.onChange(props.currentPage - 1)}
+                >
+                    {"<<"} <span className="desktop-only">PREV</span>
+                </button>
+                {renderPageButtons()}
+                <button
+                    className="next paginate-button"
+                    disabled={props.currentPage >= totalPages}
+                    onClick={() => props.onChange(props.currentPage + 1)}
+                >
+                    <span className="desktop-only">NEXT</span> {">>"}
+                </button>
+            </div>
+        </div>
+    );
+}
